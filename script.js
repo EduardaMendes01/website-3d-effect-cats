79faa7b0587d6af12e1ea8e47c69b6341a75ea4f
EduardaMendes01 <eduardamendes0100@gmail.com>
2024-06-04 04:48:22 -0300

    bug fix and make responsive

--- script.js
+++ script.js
@@ -3,10 +3,7 @@ const parallax_el = document.querySelectorAll(".parallax");
 let xValue = 0,
  yValue = 0;
 
-window.addEventListener("mousemove", (e) => {
-    xValue = e.clientX - window.innerWidth / 2;
-    yValue = e.clientY - window.innerHeight / 2;
-
+function update(cursorPosition){
     parallax_el.forEach((el) => {
         /*Horizontal*/
         let speedx = el.dataset.speedx;
@@ -18,10 +15,19 @@ window.addEventListener("mousemove", (e) => {
         let isInLeft =
             parseFloat(getComputedStyle(el).left) < window.innerWidth / 2 ? 1 : -1;
 
-        let zValue = e.clientX - parseFloat(getComputedStyle(el).left) * isInLeft * 0.01;
+        let zValue = cursorPosition - parseFloat(getComputedStyle(el).left) * isInLeft * 0.01;
             
         el.style.transform = `translateX(calc(-50% + ${
             -xValue * speedx
         }px)) translateY(calc(-50% + ${yValue * speedy}px)) perspective(5000px) translateZ(${zValue * speedz}px)`;
     });
+}
+
+update(0)
+
+window.addEventListener("mousemove", (e) => {
+    xValue = e.clientX - window.innerWidth / 2;
+    yValue = e.clientY - window.innerHeight / 2;
+
+    update(e.clientX);
 });
